3864de804f526566b742dc4b5e890afa989bc89c
Jan Wróblewski <janwrob44@edu.gdansk.pl>
2024-11-07 15:19:26 +0100

    configuracja

---
+++ Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ConsoleApp1.BibliotekaContext>
+    {
+
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ConsoleApp1.BibliotekaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+             
+        }
+    }
+}
